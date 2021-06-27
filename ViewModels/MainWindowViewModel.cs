c84842ee0a0d9874e2c503cffcc33058a497db21
Сергей Захаров <sergei86zahar@mail.ru>
2021-06-27 16:47:22 +0700

    Создание структуры проекта и ViewMolel

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,33 @@
+﻿using NEW_CV19.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NEW_CV19.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Заголовок окна
+
+        private string _title = "Анализ статистики CV19";
+
+        /// <summary>Заголовок окна</summary>
+        public string Title
+        {
+            get => _title;
+            //set
+            //{
+            //    //if (Equals(_Title, value)) return;
+            //    //_Title = value;
+            //    //OnPropertyChanged();
+
+            //    Set(ref _Title, value);
+            //}
+            set => Set(ref _title, value);
+        }
+
+        #endregion
+    }
+}
