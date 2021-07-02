f9f9ca4fc824ec619d89f25000de2d6b785b3b7e
Сергей Захаров <sergei86zahar@mail.ru>
2021-07-02 20:56:13 +0700

    Команда CloseApplicationCommand

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,15 @@
-﻿using NEW_CV19.ViewModels.Base;
+﻿using NEW_CV19.Infrastructure.Commands;
+using NEW_CV19.ViewModels.Base;
+using System.Windows;
+using System.Windows.Input;
 
 namespace NEW_CV19.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+
+        #region Заголовок окна
+
         /// <summary>Заголовок окна</summary>
         public string Title
         {
@@ -19,11 +25,11 @@ namespace NEW_CV19.ViewModels
             set => Set(ref _title, value);
         }
 
-        #region Заголовок окна
 
         private string _title = "Анализ статистики CV19";
 
         #endregion
+
         #region Status : string - Статус программы
 
         /// <summary>Статус программы</summary>
@@ -37,5 +43,32 @@ namespace NEW_CV19.ViewModels
         }
 
         #endregion
+
+        #region Команды
+
+        #region CloseApplicationCommand
+
+        public ICommand CloseApplicationCommand { get; }
+
+        private bool CanCloseApplicationCommandExecute(object p) => true;
+        private void OnCloseApplicationCommandExecuted(object p)
+        {
+            Application.Current.Shutdown();
+        }
+
+        #endregion
+
+
+
+
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            #region Команды
+            CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+
+            #endregion
+        }
     }
 }
