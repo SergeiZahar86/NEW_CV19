1867a9eaba1c736d06257e9e08df70fac31a9123
Сергей Захаров <sergei86zahar@mail.ru>
2021-07-03 07:59:39 +0700

    FontAwesome

---
+++ Infrastructure/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Windows;
+using NEW_CV19.Infrastructure.Commands.Base;
+
+namespace NEW_CV19.Infrastructure.Commands
+{
+    internal class CloseApplicationCommand : Command
+    {
+        public override bool CanExecute(object parameter) => true;
+
+        public override void Execute(object parameter) => Application.Current.Shutdown();
+    }
+}
