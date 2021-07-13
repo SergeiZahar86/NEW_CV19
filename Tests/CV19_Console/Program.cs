3a94ac49a372d922e2caa8fef42d248a8bedb581
Сергей Захаров <sergei86zahar@mail.ru>
2021-07-14 01:44:14 +0700

    1

--- Tests/CV19_Console/Program.cs
+++ Tests/CV19_Console/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,7 @@ namespace CV19_Console
     class Program
     {
         private const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
-        
+
         private static async Task<Stream> GetDataStream()
         {
             var client = new HttpClient();
@@ -38,14 +39,15 @@ namespace CV19_Console
         
         static void Main(string[] args)
         {
-            var client = new HttpClient();
-            var respons = client.GetAsync(data_url).Result;
-            var csv_str = respons.Content.ReadAsStringAsync().Result;
+            //var client = new HttpClient();
+            //var respons = client.GetAsync(data_url).Result;
+            //var csv_str = respons.Content.ReadAsStringAsync().Result;
+
+            foreach (var data_line in GetDataLines())
+                Console.WriteLine(data_line);
 
 
-
-
-            Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
