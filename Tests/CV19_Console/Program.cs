1543d4370f8abf25bfb657c3e8dd8ffd3c2267ef
Сергей Захаров <sergei86zahar@mail.ru>
2021-07-23 15:14:23 +0700

    1

--- Tests/CV19_Console/Program.cs
+++ Tests/CV19_Console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -12,14 +13,20 @@ namespace CV19_Console
     class Program
     {
         private const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
-
+        /// <summary>
+        /// Создает поток данных
+        /// </summary>
+        /// <returns></returns>
         private static async Task<Stream> GetDataStream()
         {
             var client = new HttpClient();
             var respons = await client.GetAsync(data_url, HttpCompletionOption.ResponseHeadersRead);
             return await respons.Content.ReadAsStreamAsync();
         }
-        
+        /// <summary>
+        /// Разбиват поток данных на отдельные строки
+        /// </summary>
+        /// <returns></returns>
         private static IEnumerable<string> GetDataLines()
         {
             using (var data_stream = GetDataStream().Result)
@@ -35,17 +42,30 @@ namespace CV19_Console
                 }
             }
         }
-        
-        
+        /// <summary>
+        /// Получает все даты из файла
+        /// </summary>
+        /// <returns></returns>
+        private static DateTime[] GetDates()
+        {
+            return GetDataLines()
+                .First()
+                .Split(',')
+                .Skip(4)
+                .Select(s => DateTime.Parse(s, CultureInfo.InvariantCulture))
+                .ToArray();
+        }
+
         static void Main(string[] args)
         {
             //var client = new HttpClient();
             //var respons = client.GetAsync(data_url).Result;
             //var csv_str = respons.Content.ReadAsStringAsync().Result;
 
-            foreach (var data_line in GetDataLines())
-                Console.WriteLine(data_line);
-
+            /* foreach (var data_line in GetDataLines())
+                 Console.WriteLine(data_line);*/
+            var dates = GetDates();
+            Console.WriteLine(string.Join("\r\n", dates));
 
             Console.ReadLine();
         }
