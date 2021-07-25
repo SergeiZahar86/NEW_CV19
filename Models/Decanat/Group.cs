7c0335fa83d48049f73fde5eca7e3e2fdf0cca46
Сергей Захаров <sergei86zahar@mail.ru>
2021-07-25 23:26:30 +0700

    1

---
+++ Models/Decanat/Group.cs
@@ -0,0 +1,16 @@
+﻿using NEW_CV19.Models.Interfaces;
+using System.Collections.Generic;
+
+namespace NEW_CV19.Models.Decanat
+{
+    internal class Group : IEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public IList<Student> Students { get; set; } = new List<Student>();
+
+        public string Description { get; set; }
+    }
+}
---
+++ Models/Decanat/Student.cs
@@ -0,0 +1,22 @@
+﻿using NEW_CV19.Models.Interfaces;
+using System;
+
+namespace NEW_CV19.Models.Decanat
+{
+    internal class Student : IEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Patronymic { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public double Rating { get; set; }
+
+        public string Description { get; set; }
+    }
+}
