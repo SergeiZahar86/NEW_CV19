779e087058fee493553b87aa5bfa7d1583fb9da2
Сергей Захаров <sergei86zahar@mail.ru>
2021-09-15 21:41:19 +0700

    1

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,15 +1,103 @@
 ﻿using NEW_CV19.Infrastructure.Commands;
 using NEW_CV19.Models;
+using NEW_CV19.Models.Decanat;
 using NEW_CV19.ViewModels.Base;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
 using System.Windows;
+using System.Windows.Data;
 using System.Windows.Input;
 
 namespace NEW_CV19.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+
+        public ObservableCollection<Group> Groups { get; }
+
+
+        #region SelectedGroup : Group - Выбранная группа студентов
+        /// <summary>Выбранная группа студентов</summary>
+        private Group _SelectedGroup;
+        /// <summary>Выбранная группа студентов</summary>
+        public Group SelectedGroup
+        {
+            get => _SelectedGroup;
+            set => Set(ref _SelectedGroup, value);
+        }
+        #endregion
+
+        #region SelectedStudent : Student - Выбранный студент
+        /// <summary>Выбранный студент</summary>
+        private Student _SelectedStudent;
+        /// <summary>Выбранный студент</summary>
+        public Student SelectedStudent
+        {
+            get => _SelectedStudent;
+            set => Set(ref _SelectedStudent, value);
+        }
+        #endregion
+
+
+
+
+        #region StudentFilterText : string - Текст фильтра студентов
+
+        /// <summary>Текст фильтра студентов</summary>
+        private string _StudentFilterText;
+
+        /// <summary>Текст фильтра студентов</summary>
+        public string StudentFilterText
+        {
+            get => _StudentFilterText;
+            set
+            {
+                if (!Set(ref _StudentFilterText, value)) return;
+                _SelectedGroupStudents.View.Refresh();
+            }
+        }
+
+        #endregion
+
+        #region SelectedGroupStudents
+
+        private readonly CollectionViewSource _SelectedGroupStudents = new CollectionViewSource();
+
+        private void OnStudentFiltred(object Sender, FilterEventArgs E)
+        {
+            if (!(E.Item is Student student))
+            {
+                E.Accepted = false;
+                return;
+            }
+
+            var filter_text = _StudentFilterText;
+            if (string.IsNullOrWhiteSpace(filter_text))
+                return;
+
+            if (student.Name is null || student.Surname is null || student.Patronymic is null)
+            {
+                E.Accepted = false;
+                return;
+            }
+
+            //if (student.Name.Contains(filter_text, StringComparison.OrdinalIgnoreCase)) return;
+            //if (student.Surname.Contains(filter_text, StringComparison.OrdinalIgnoreCase)) return;
+            //if (student.Patronymic.Contains(filter_text, StringComparison.OrdinalIgnoreCase)) return;
+
+            E.Accepted = false;
+        }
+
+        public ICollectionView SelectedGroupStudents => _SelectedGroupStudents?.View;
+
+        #endregion
+
+
+
+
         #region SelectedPageIndex : int - Номер выбранной вкладки
 
         /// <summary>Номер выбранной вкладки</summary>
@@ -110,8 +198,24 @@ namespace NEW_CV19.ViewModels
 
                 data_points.Add(new DataPoint { XValue = x, YValue = y });
             }
-
             TestDataPoints = data_points;
+
+
+            var student_index = 1;
+            var students = Enumerable.Range(1, 10).Select(x => new Student 
+            {
+                Name = $"Name {student_index}",
+                Surname = $"Surname {student_index}",
+                Patronymic = $"Patronymic {student_index++}",
+                Birthday = DateTime.Now,
+                Rating = 0
+            });
+            var groups = Enumerable.Range(1, 20).Select(x => new Group
+            {
+                Name = $"Группа {x}",
+                Students = new ObservableCollection<Student>(students)
+            });
+            Groups = new ObservableCollection<Group>(groups);
         }
     }
 }
